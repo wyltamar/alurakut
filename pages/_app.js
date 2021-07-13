2d6e22c5c9fa5436a0ddf23a476febc42f60c279
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-07-12 23:29:06 -0300

    near finish css

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,33 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Reset css*/
+  *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-`
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
+  }
+
+  #__next{
+    display: flax;
+    min-heigth: 100vh;
+    flex-direction: column;
+  }
+`;
 
 const theme = {
   colors: {
     primary: '#0070f3',
   },
-}
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +37,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,45 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `;
 
+const Box = styled.div`
+  background: #ffffff;
+  border-radius: 8px;
+`;
+
+const MainGrid = styled.main`
+  display: grid;
+  grid-gab: 10px;
+  padding: 16px;
+
+  @media (min-width: 860px) {
+    grid-template-area: 'profileArea welcomeArea profileRelationsArea';
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
 export default function Home() {
-  return <Title>My page</Title>
+  //prettier-ignore
+  return (
+  <MainGrid>
+    <div style={{gridArea: "profileArea"}}>
+    <Box >
+    Imagem
+    </Box>
+    </div>
+    <div style={{gridArea: "welcomeArea"}}>
+    <Box>
+    Bem vindo
+    </Box>
+    </div>
+    <div style={{gridArea: "profileRelationsArea"}}>
+    <Box >
+    Pessoas da comunidade
+    </Box>
+    </div>
+
+  </MainGrid>
+  );
 }
